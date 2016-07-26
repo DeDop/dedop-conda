fe583b9145cc51cef807337187eb539f80c8634a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-07-26 15:03:27 +0200

    avoid passing arguments to dedop-shell.bat

--- recipe/dedop-shell.bat
+++ recipe/dedop-shell.bat
@@ -1,14 +1,19 @@
 @echo off
 
-set ROOT_PREFIX=%1
-set PREFIX=%2
+set SCRIPTS_DIR=%~dp0
+
+pushd .
+cd %SCRIPTS_DIR%
+set PREFIX=%~f1
+popd
+
+call "%PREFIX%\Scripts\activate.bat" "%PREFIX%"
+if errorlevel 1 exit 1
+
+prompt $G$S
 
 echo.
 @echo Welcome to the DeDop Shell. Type "dedop -h" to get help.
 echo.
-
-call "%ROOT_PREFIX%\Scripts\activate.bat" "%PREFIX%"
-
-prompt $G$S
 
 cmd /K ""
