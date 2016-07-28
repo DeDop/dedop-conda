1ceae002aca1b2e4de6d7d59e378e2873844f7f3
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2016-07-28 10:31:37 +0200

    use default terminal app

--- recipe/dedop-shell.app/Contents/MacOS/launch-dedop-shell-in-terminal.sh
+++ recipe/dedop-shell.app/Contents/MacOS/launch-dedop-shell-in-terminal.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DEDOP_BIN="$( cd "${DIR}/../../.."  && pwd )"
 
-open -a Terminal "${DEDOP_BIN}/dedop-shell.command"
+open "${DEDOP_BIN}/dedop-shell.command"
