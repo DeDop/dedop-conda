6f06a3042bfdc6069657da4bd20eb2ac4cdcebc3
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2016-07-28 10:32:01 +0200

    get version corretcly

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -2,10 +2,8 @@
 
 # http://conda.pydata.org/docs/building/build-scripts.html
 
-if [ `uname` == Darwin ]
+if [ `uname` != Darwin ]
 then
-    sed -i -e "s,\${PKG_VERSION},${PKG_VERSION},g"  "${PREFIX}/bin/dedop-shell.app/Contents/Info.plist"
-else
     APP_DIR="${HOME}/.local/share/applications"
     DESKTOP_TEMPLATE="${PREFIX}/share/dedop/dedop.desktop-template"
 
